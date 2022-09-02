1669d7001a20a81d8f9a1f3fee3eb562d5457d08
Gisithj <gisithj@gmail.com>
2022-09-03 01:10:50 +0530

    text field styles

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ app.get("/sign-up",function(req,res){
     res.sendFile(__dirname + "/views/signup.html")
 })
 
-app.listen(3000,function(){
+app.listen(3001,function(){
     console.log("Server is running on port 3000");
 })
