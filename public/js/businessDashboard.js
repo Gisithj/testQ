33c48b5156be9349d2e0b908d219ae66506aa044
kavinda <110517432+kmkavinda6@users.noreply.github.com>
2022-09-19 05:58:18 +0530

    create q tbody

--- public/js/businessDashboard.js
+++ public/js/businessDashboard.js
@@ -11,9 +11,9 @@ $(document).ready(function(){
       }
     })
 
-    // $("tbody tr").click(function() {
-    //     window.location = '/businessDashboard/createQueue';
-    //   });
+     $("tbody tr").click(function() {
+        window.location = '/businessDashboard/Openqueue';
+      });
 
 
       
